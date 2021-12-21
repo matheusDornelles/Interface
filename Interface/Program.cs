f6eae29bef1511eed2a0e6e212db196007e4912f
Matheus <mdmaia91@gmail.com>
2021-12-20 21:43:21 -0300

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Interface;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+
+            Animal animal = new Animal();
+
+            animal.Nome = "Rex";
+            animal.NomeDono= "Pedro";
+            animal.Especie = "Cachorro";
+
+        }
+    }    
+}
+
+
